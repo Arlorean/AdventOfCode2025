9bdd4ee90d4870c6f3ff5581227691ffd745171b
Adam Davidson <adam@adam-davidson.com>
2025-12-03 13:29:02 +0000

    Initial commit

---
+++ Day1/Program.fs
@@ -0,0 +1,34 @@
+﻿open System
+open System.IO
+open FParsec
+
+let parser = anyOf ['L'; 'R'] .>>. pint32 .>> eof
+
+let parseTurn s =
+    match run parser s with
+    | Success (('L',n),_,_) -> Some +n
+    | Success (('R',n),_,_) -> Some -n
+    | _ -> None
+
+let applyTurn (position:int) (clicks:int) = (position+clicks)%100 
+
+let getPassword startPosition inputFile = 
+    File.ReadLines inputFile
+    |> Seq.choose parseTurn
+    |> Seq.scan applyTurn startPosition
+    |> Seq.filter ((=) 0)
+    |> Seq.length
+
+printfn "Part 1 Result: %d" (getPassword 50 "input.txt")
+
+let turnToClicks (clicks:int) = Seq.replicate (Math.Abs clicks) (Math.Sign clicks)
+            
+let getPassword2 startPosition inputFile = 
+    File.ReadLines inputFile
+    |> Seq.choose parseTurn
+    |> Seq.collect turnToClicks
+    |> Seq.scan applyTurn startPosition
+    |> Seq.filter ((=) 0)
+    |> Seq.length
+
+printfn "Part 2 Result: %d" (getPassword2 50 "input.txt")
---
+++ README.md
@@ -0,0 +1,2 @@
+# AdventOfCode2025
+F# solutions to the Advent of Code 2025 challenge
