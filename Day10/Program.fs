ba790de6b809f69c874f2d85807b79eed49403a2
Adam Davidson <adam@adam-davidson.com>
2025-12-11 01:00:54 +0000

    Parse directly into Machine record type. Build int bit patterns for lights/buttons

--- Day10/Program.fs
+++ Day10/Program.fs
@@ -2,6 +2,30 @@
 open System.IO
 open FParsec
 
+type Machine = {
+    Lights: int
+    Buttons: int list
+    Joltage: int list
+} with
+    static member Create lights  buttons joltage=
+        { Lights = lights; Buttons = buttons; Joltage = joltage }
+
+let inline flip f x y = f y x
+
+let bitIndicesToInt =
+    Seq.map ((<<<) 1) 
+    >> Seq.fold (+) 0
+
+let bitsToInt =
+    Seq.mapi (flip (<<<))
+    >> Seq.fold (+) 0
+
+let lightToBit = function
+    | '.' -> 0
+    | '#' -> 1
+    | _   -> failwith "invalid light state"
+
+
 let token p = p .>> spaces
 
 let number  = token pint32
@@ -13,21 +37,21 @@ let rbrack  = token (pchar ']')
 let lbrace  = token (pchar '{')
 let rbrace  = token (pchar '}')
 
-let lights = between lbrack rbrack (many (anyOf ['.'; '#']))
-let circuit = between lparen rparen (sepBy1 number comma)
-let circuits = many1 circuit
+let light = anyOf ['.'; '#'] |>> lightToBit
+let lights = between lbrack rbrack (many light) |>> bitsToInt
+let button = between lparen rparen (sepBy1 number comma) |>> bitIndicesToInt
+let buttons = many1 button
 let joltage = between lbrace rbrace (sepBy1 number comma)
-let machine = lights >>. circuits >>. joltage .>> eof
+let machine = pipe3 lights buttons joltage Machine.Create .>> eof
 
 let parseMachine s =
     match run machine s with
-    | Success (v,_,_) -> v
-    | Failure (error, _, _) ->
-        failwithf "Parsing error: %s" error
+    | Success (machine,_,_) -> machine
+    | Failure (error,_,_) -> failwith error
 
 let result1 input =
     File.ReadLines(input)
-    |> Seq.filter (String.IsNullOrWhiteSpace >> not)
+    |> Seq.filter (not << String.IsNullOrWhiteSpace)
     |> Seq.map parseMachine
 
 printfn "Part 1 Example: %A" (result1 "example.txt")
