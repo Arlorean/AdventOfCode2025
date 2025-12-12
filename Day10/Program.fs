5c83e43f17bf9c9efd21bab788086b44ced4c38d
Adam Davidson <adam@adam-davidson.com>
2025-12-12 10:14:41 +0000

    Day 10 Part 2 solution but only performant enough for the example, not the actual input

--- Day10/Program.fs
+++ Day10/Program.fs
@@ -4,40 +4,11 @@ open FParsec
 
 type Machine = {
     Lights: int
-    Buttons: int list
-    Joltage: int list
+    Buttons: int [][]
+    Joltages: int []
 } with
-    static member Create lights  buttons joltage=
-        { Lights = lights; Buttons = buttons; Joltage = joltage }
-
-let formatLights n lights =
-    seq {
-        yield "["
-        for i in 0 .. n do
-            if (lights &&& (1 <<< i)) <> 0 then
-                yield "#"
-            else
-                yield "."
-        yield "]"
-    } |> String.Concat
-
-let formatButton button =
-    seq {
-        yield "("
-        yield String.Join(",",
-            seq {
-                for i: int32 in 0 .. 31 do
-                    if (button &&& (1 <<< i)) <> 0 then
-                        yield i.ToString()
-            }
-        )
-        yield ")"
-    } |> String.Concat
-
-let formatButtons buttons =
-    buttons
-    |> List.map formatButton
-    |> String.concat " "
+    static member Create lights  buttons joltages=
+        { Lights = lights; Buttons = buttons; Joltages = joltages }
 
 
 let inline flip f x y = f y x
@@ -69,9 +40,9 @@ let rbrace  = token (pchar '}')
 
 let light = anyOf ['.'; '#'] |>> lightToBit
 let lights = between lbrack rbrack (many light) |>> bitsToInt
-let button = between lparen rparen (sepBy1 number comma) |>> bitIndicesToInt
-let buttons = many1 button
-let joltage = between lbrace rbrace (sepBy1 number comma)
+let button = between lparen rparen (sepBy1 number comma) |>> List.toArray
+let buttons = many1 button |>> List.toArray
+let joltage = between lbrace rbrace (sepBy1 number comma) |>> List.toArray
 let machine = pipe3 lights buttons joltage Machine.Create .>> eof
 
 let parseMachine s =
@@ -85,43 +56,120 @@ let parseMachinesFile input =
     |> Seq.map parseMachine
 
 
-module List =
-    let combinations xs =
-        let n = List.length xs
+let combinations elements =
+    let n = Array.length elements
+    let rec choose k start = seq {
+        if k = 0 then yield []
+        elif start < n then
+            for i in start .. n - 1 do
+                for tail in choose (k - 1) (i + 1) do
+                    yield elements[i] :: tail
+    }
+    seq {
+        for k in 0 .. n do
+            yield! choose k 0
+    }
 
-        let rec choose k start = seq {
-            if k = 0 then yield []
-            elif start < n then
-                for i in start .. n - 1 do
-                    for tail in choose (k - 1) (i + 1) do
-                        yield xs[i] :: tail
-        }
-
-        seq {
-            for k in 0 .. n do
-                yield! choose k 0
-        }
 
 let buttonCombinations machine =
     machine.Buttons
-    |> List.combinations
+    |> combinations
 
-let pressButton lightState buttonLightsToggle  =
-    lightState ^^^ buttonLightsToggle 
+let pressButton lightState buttonLightToggles =
+    lightState ^^^ (bitIndicesToInt buttonLightToggles)
 
 let pressButtons = List.fold pressButton 0
 
-let machineResult machine =
+let machineResult1 machine =
     buttonCombinations machine
     |> Seq.find (pressButtons >> (=) machine.Lights)
 
 let result1 input =
     parseMachinesFile input
-    |> Seq.map machineResult
+    |> Seq.map machineResult1
     |> Seq.map List.length
     |> Seq.fold (+) 0
 
 
 printfn "Part 1 Example: %A" (result1 "example.txt")
 printfn "Part 1 Result: %A" (result1 "input.txt")
+
+let distributeByCount count elements =
+    let rec distribute remaining values acc =
+        match values with
+        | [] -> 
+            if remaining = 0 then [List.rev acc]
+            else []
+        | v :: rest ->
+            [0 .. remaining]
+            |> List.collect (fun count ->
+                let newAcc = if count > 0 then (v, count) :: acc else acc
+                distribute (remaining - count) rest newAcc
+            )
+    
+    distribute count elements []
+
+let choiceCombinations (choices: 'a list array) =
+    let rec loop idx acc = seq {
+        if idx = choices.Length then
+            // we've picked one from each slot
+            yield List.rev acc
+        else
+            // try every option in this slot
+            for choice in choices[idx] do
+                yield! loop (idx + 1) (choice :: acc)
+    }
+    loop 0 []
+
+
+let buttonIndicesAffectingJoltage machine joltageIndex  =
+    machine.Buttons
+    |> Array.indexed
+    |> Array.filter (Array.exists ((=) joltageIndex) << snd)
+    |> Array.map fst
+
+let joltageButtonCombinations machine joltageIndex =
+    buttonIndicesAffectingJoltage machine joltageIndex
+    |> Array.toList
+    |> distributeByCount (machine.Joltages[joltageIndex]) 
+
+let isValidSolution machine (buttonPresses : (int*int) list)  =
+    let hasConflictingPressCounts = 
+        buttonPresses
+        |> List.groupBy fst
+        |> List.map snd
+        |> List.map List.length
+        |> List.exists ((>) 1)
+
+    if hasConflictingPressCounts then
+        false
+    else
+        let mutable counts = Array.zeroCreate machine.Joltages.Length
+        buttonPresses
+            |> List.iter (fun (buttonIndex, pressCount) ->
+                let buttonJoltages =
+                    machine.Buttons[buttonIndex]
+                buttonJoltages
+                |> Array.iter (fun joltageIndex ->
+                    counts[joltageIndex] <- counts[joltageIndex] + pressCount
+                )
+            )
+        Array.forall2 (=) counts machine.Joltages
+
+let machineResult2 machine =
+    [|0..machine.Joltages.Length - 1|]
+    |> Array.map (joltageButtonCombinations machine)
+    |> choiceCombinations
+    |> Seq.map List.concat
+    |> Seq.map List.distinct
+    |> Seq.filter (isValidSolution machine)
+    |> Seq.map (List.sumBy snd)
+    |> Seq.min
+
+let result2 input = 
+    parseMachinesFile input
+    |> Seq.map machineResult2
+
+printfn "Part 2 Example: %A" (result2 "example.txt")
+//printfn "Part 2 Result: %A" (result2 "input.txt")
 
