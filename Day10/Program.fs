810863131be37b04be36041c7b32d6687166af71
Adam Davidson <adam@adam-davidson.com>
2025-12-13 13:00:18 +0000

    Day 10 Part 2 Solution using Z3

--- Day10/Program.fs
+++ Day10/Program.fs
@@ -97,131 +97,12 @@ let result1 input =
 printfn "Part 1 Example: %A" (result1 "example.txt")
 printfn "Part 1 Result: %A" (result1 "input.txt")
 
-let distributeByCount count elements =
-    let rec distribute remaining values acc =
-        match values with
-        | [] -> 
-            if remaining = 0 then [List.rev acc]
-            else []
-        | v :: rest ->
-            [0 .. remaining]
-            |> List.collect (fun count ->
-                let newAcc = if count > 0 then (v, count) :: acc else acc
-                distribute (remaining - count) rest newAcc
-            )
-    
-    distribute count elements []
-
-let choiceCombinations (choices: 'a list array) =
-    let rec loop idx acc = seq {
-        if idx = choices.Length then
-            // we've picked one from each slot
-            yield List.rev acc
-        else
-            // try every option in this slot
-            for choice in choices[idx] do
-                yield! loop (idx + 1) (choice :: acc)
-    }
-    loop 0 []
-
 
 let buttonIndicesAffectingJoltage machine joltageIndex  =
     machine.Buttons
     |> Array.indexed
     |> Array.filter (Array.exists ((=) joltageIndex) << snd)
     |> Array.map fst
-
-let joltageButtonCombinations machine joltageIndex =
-    buttonIndicesAffectingJoltage machine joltageIndex
-    |> Array.toList
-    |> distributeByCount (machine.Joltages[joltageIndex]) 
-
-let isValidSolution machine (buttonPresses : (int*int) list)  =
-    let hasConflictingPressCounts = 
-        buttonPresses
-        |> List.groupBy fst
-        |> List.map snd
-        |> List.map List.length
-        |> List.exists ((>) 1)
-
-    if hasConflictingPressCounts then
-        false
-    else
-        let mutable counts = Array.zeroCreate machine.Joltages.Length
-        buttonPresses
-            |> List.iter (fun (buttonIndex, pressCount) ->
-                let buttonJoltages =
-                    machine.Buttons[buttonIndex]
-                buttonJoltages
-                |> Array.iter (fun joltageIndex ->
-                    counts[joltageIndex] <- counts[joltageIndex] + pressCount
-                )
-            )
-        Array.forall2 (=) counts machine.Joltages
-
-let machineResult2 machine =
-    [|0..machine.Joltages.Length - 1|]
-    |> Array.map (joltageButtonCombinations machine)
-    |> choiceCombinations
-    |> Seq.map List.concat
-    |> Seq.map List.distinct
-    |> Seq.filter (isValidSolution machine)
-    |> Seq.map (List.sumBy snd)
-    |> Seq.min
-
-let result2 input = 
-    parseMachinesFile input
-    |> Seq.map machineResult2
-
-//printfn "Part 2 Example: %A" (result2 "example.txt")
-//printfn "Part 2 Result: %A" (result2 "input.txt")
-
-// 3 = e + f
-// 5 = b + f
-// 4 = c + d + e
-// 7 = a + b + d
-
-let test=
-    let a = Int("a")
-    let b = Int("b")
-    let c = Int("c")
-    let d = Int("d")
-    let e = Int("e")
-    let f = Int("f")
-    let sum = Int("sum")
-
-    let constraints = [|
-        a >=. 0I
-        b >=. 0I
-        c >=. 0I
-        d >=. 0I
-        e >=. 0I
-        f >=. 0I
-
-        e + f =. 3I
-        b + f =. 5I
-        c + d + e =. 4I
-        a + b + d =. 7I
-
-        sum =. a + b + c + d + e + f
-    |]
-    let constraintExprs = 
-        constraints
-        |> Array.map (fun c -> c.Expr :?> BoolExpr)
-
-    let optimizer = Gs.context().MkOptimize()
-    optimizer.Add constraintExprs
-    optimizer.MkMinimize(sum.Expr :?> ArithExpr) |> ignore
-
-    match optimizer.Check() with
-    | Status.SATISFIABLE ->
-        let model = optimizer.Model
-        printfn "Z3 Success: %O" (model.Evaluate sum.Expr)
-    | failure ->
-        printfn "Z3 Failure: %O" failure
-
-test |> ignore
-
 
 let toArithExpr (expr:Expr) = expr :?> ArithExpr
 
@@ -230,7 +111,7 @@ let solve machine =
     let optimizer = context.MkOptimize()
 
     let mkButtonVar buttonIndex =
-        let var = context.MkInt($"button_{buttonIndex}")
+        let var = context.MkIntConst($"button_{buttonIndex}")
         context.MkGe(var, context.MkInt(0)) |> optimizer.Add
         var
 
@@ -271,9 +152,11 @@ let solve machine =
     | _ ->
         None
 
-let resultz input = 
+let result2 input = 
     parseMachinesFile input
     |> Seq.map solve
+    |> Seq.choose id
+    |> Seq.sum
 
-resultz "example.txt"
-    |> Seq.iter (printfn "%A")
+printfn "Part 2 Example: %A" (result2 "example.txt")
+printfn "Part 2 Result: %A" (result2 "input.txt")
